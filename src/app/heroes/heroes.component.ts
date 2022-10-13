50094b133ac5c27f152eaaf9fe36026c08300d8d
Martin <martinsolera71@gmail.com>
2022-10-13 12:05:03 -0300

    Finalizado paso-05

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,7 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
